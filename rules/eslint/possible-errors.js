742275a9b3e6d7bebfa50f667a1d5b551b2c468e
thiagogarbazza <thiagogarbazza@gmail.com>
2016-12-13 11:14:46 -0200

    Separating rules possible-erros

---
+++ rules/eslint/possible-errors.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  "rules": {
+    "no-await-in-loop": "error",
+    "no-extra-parens": "error",
+    "no-prototype-builtins": "error",
+    "no-template-curly-in-string": "error",
+    "no-unsafe-negation": "error",
+    "valid-jsdoc": "warn",
+  }
+};
