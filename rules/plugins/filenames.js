9575a169361939ccf9acf2d62055a54700506912
thiagogarbazza <thiagogarbazza@gmail.com>
2016-12-13 11:15:33 -0200

    Separating rules filenames

---
+++ rules/plugins/filenames.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  "plugins": ["filenames"],
+  "rules": {
+    "filenames/match-exported": ["error", "kebab"],
+    "filenames/match-regex": ["error", "^[a-z-]+$", true],
+    "filenames/no-index": "error",
+  }
+};
