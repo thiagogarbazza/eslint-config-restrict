17284d8b85543a931dfdc1a62a6d2ff1e13aa7d3
thiagogarbazza <thiagogarbazza@gmail.com>
2016-12-14 10:02:22 -0200

    Separating rules ecma-script-6

---
+++ rules/eslint/ecma-script-6.js
@@ -0,0 +1,30 @@
+'use strict'
+
+module.exports = {
+  "rules": {
+    "arrow-body-style": ["warn", "as-needed"],
+    "arrow-parens": ["error", "as-needed"],
+    "arrow-spacing": "warn",
+    "generator-star-spacing": ["warn", {"before": true, "after": false}],
+    "no-confusing-arrow": ["error", {"allowParens": true}],
+    "no-duplicate-imports":  ["error", { "includeExports": true }],
+    "no-new-symbol": "error",
+    // "no-restricted-imports": ["error", "fs"]
+    "no-useless-computed-key": "error",
+    "no-useless-constructor": "error",
+    "no-useless-rename": "warn",
+    "no-var": "error",
+    "object-shorthand": ["error", "always", {"avoidQuotes": true}],
+    "prefer-arrow-callback": "warn",
+    "prefer-const": "warn",
+    "prefer-numeric-literals": "error",
+    "prefer-rest-params": "warn",
+    "prefer-spread": "warn",
+    "prefer-template": "warn",
+    "rest-spread-spacing": ["warn", "never"],
+    "sort-imports": "warn",
+    "symbol-description": "error",
+    "template-curly-spacing": "warn",
+    "yield-star-spacing": ["warn", "before"]
+  }
+};
